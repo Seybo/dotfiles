879486b5f3a673ffd79579742ae5ae561490e240
seybo <michaelatworkspace@gmail.com>
2020-07-24 09:19:40 +0530

    add aliases.sh

---
+++ .myconfig/aliases.sh
@@ -0,0 +1,110 @@
+####### tmux ########
+alias tmux="tmux -u"
+alias ts='tmux new-session -s'
+alias tl='tmux list-sessions'
+alias tksv='tmux kill-server'
+alias tkss='tmux kill-session -t'
+# alias tsave='tmux2html'
+tsave() {
+  # $1 - window/pane (0.1 or 2 or 2.3)
+  # $2 - history limit
+  LINES=${2:-100}
+  tmux2html "$1" --history "$LINES" -o ~/tmux.html
+}
+alias tshow='xopen ~/tmux.html &> /dev/null &'
+
+####### zsh ########
+alias szsh="source ~/.zshrc"
+alias k9="kill -9"
+alias t="tree"
+alias rboot="sudo reboot"
+alias pwoff="sudo poweroff"
+alias rp="realpath"
+
+
+####### git ########
+alias gpf='git push --force-with-lease'
+alias gca='git commit --amend --no-edit'
+alias gcae='git commit --amend'
+alias gitc='git commit -v -m "'
+alias gbD='git branch -D'
+alias gc-='git checkout -'
+alias gaf='gaa && gca && gpf'
+alias gurbm='grbm && gcm && gup && gc- && grbm' # first rebase is for unstashed changes notification
+alias grsh='git reset --soft HEAD~1'
+alias gsta='git stash save' # overwriting oh-my-zsh that uses git stash push
+alias gcms='gsta && gcm && gstp' # checkout master with stash
+alias gc-s='gsta && gc- && gstp' # checkout last branch with stash
+alias gstk='git stash --keep-index --include-untracked'
+
+
+####### dotfiles ########
+alias dpf='dfiles push --force-with-lease'
+alias dca='dfiles commit --amend --no-edit'
+alias dcae='dfiles commit --amend'
+alias ditc='dfiles commit -v -m "'
+alias dst='dfiles status'
+alias dan='dfiles add'
+alias daa='dfiles add -u'
+alias daf='daa && dca && dpf'
+alias ddif='dfiles diff'
+alias dlog='dfiles log'
+
+
+####### rails ########
+alias rsw='rails s webrick'
+alias rswc='rails s webrick -e test -p 5002'
+alias rsp='PORT=3000 bundle exec puma -C config/puma.rb'
+alias beg='bundle exec guard'
+alias ber='bundle exec rspec'
+# alias forber='for i in `seq 50` ; do ber '
+alias bec='bundle exec'
+alias rdbm='rails db:migrate && RAILS_ENV=test rails db:reset'
+alias rdbr='rails db:rollback && RAILS_ENV=test rails db:reset'
+alias rdbr2='rails db:rollback STEP=2 && RAILS_ENV=test rails db:reset'
+alias rdbr3='rails db:rollback STEP=3 && RAILS_ENV=test rails db:reset'
+alias wai='whereami'
+alias rlc='rails log:clear'
+# alias redisrepair= 'sudo /etc/init.d/redis_6379 stop && sudo rm /var/redis/6379/dump.rdb && sudo rm /var/run/redis_6379.pid && sudo /etc/init.d/redis_6379 start'
+
+
+####### elixir ########
+alias iex='rlwrap -a iex'
+
+
+####### front ########
+alias yasbj="yarn start storybook.js"
+alias yasbr="yarn start storybook.re"
+alias yas="yarn start build.dev.server.watch"
+alias yac="yarn start build.dev.client.watch"
+alias yasc="npx concurrently --kill-others --prefix \"[{name}]\" --names \"CLIENT_BUNDLES,SERVER_BUNDLE\" \"nps build.dev.client.watch\" \"nps build.dev.server.watch\""
+alias yar="yarn start build.bs.dev.watch"
+alias jst="TZ=UTC BABEL_ENV=jest yarn jest"
+
+
+####### files ########
+alias foal='vim ~/.myconfig/aliases.sh'
+alias fovi='vim ~/.myconfig/vim/init.vim'
+
+
+####### apps ########
+alias v='vim'
+alias rr='ranger'
+alias gls='glances'
+
+# alacritty doesn't go fullscreen with F11
+alias alf='wmctrl -r '0:1:zsh' -b toggle,fullscreen'
+
+
+####### system ########
+alias uninstall='sudo apt purge --auto-remove'
+alias xopen='xdg-open'
+alias syslog="vim /var/log/syslog"
+alias syslogclear="sudo sh -c 'cat /dev/null > /var/log/syslog'\n: 1557288145:0;sudo sh -c 'cat /dev/null > /var/log/syslog'"
+# keyboard off/on
+# alias kf='xinput set-int-prop 16 "Device Enabled" 8 0'
+# alias kn='xinput set-int-prop 16 "Device Enabled" 8 1'
+# alias sshot2='flameshot gui -d 2000'
+alias lc='colorls'
+alias lca='colorls -a'
+alias lcr='colorls -lA --sd'
