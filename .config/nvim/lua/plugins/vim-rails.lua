762cc2d58eaf4dd7c39c08a05b6eba0fb89baad1
seybo <michaelatworkspace@gmail.com>
2023-11-06 16:12:58 +0530

    update rails fixtures/factories binding

--- .config/nvim/lua/plugins/vim-rails.lua
+++ .config/nvim/lua/plugins/vim-rails.lua
@@ -4,11 +4,11 @@ function M.setup()
     map { "ra", "", "ra", mode = "n" } -- revert the original binding replaced with :A
     map { "<Leader>ra", "Rails Alternative file", ":A<CR>", mode = "n" }
     map { "<Leader>rs", "Rails Alternative file", ":R<CR>", mode = "n" }
-    map { "<Leader>rb",
+    map { "<Leader>gf",
         "TODO:",
         'viw"sy:Efixtures <C-r>=tolower(substitute(substitute(@s, \'\\n\', \'\', \'g\'), \'/\', \'\\\\/\', \'g\'))<cr>_factories<cr>',
         mode = "n" }
-    map { "<Leader>rb",
+    map { "<Leader>gf",
         "TODO:",
         '"sy:Efixtures <C-r>=tolower(substitute(substitute(@s, \'\\n\', \'\', \'g\'), \'/\', \'\\\\/\', \'g\'))<cr>',
         mode = "v" }
